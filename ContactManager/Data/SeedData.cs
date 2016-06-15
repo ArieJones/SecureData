25e051521aa38ee3bb3e328018e3a0ec6a1cf7f6
riande@microsoft.com <riande@microsoft.com>
2016-06-14 17:56:52 -0700

    make uid sync so it's available in next call

--- ContactManager/Data/SeedData.cs
+++ ContactManager/Data/SeedData.cs
@@ -20,28 +20,27 @@ namespace ContactManager.Data
             using (var context = new ApplicationDbContext(
                 serviceProvider.GetRequiredService<DbContextOptions<ApplicationDbContext>>()))
             {
-                var uid = await CreateTestUser(serviceProvider, testUserPw);
+                var uid =  CreateTestUser(serviceProvider, testUserPw);
                 await CreateCanDeleteRole(serviceProvider, uid, canDeleteRole);
-                AddRole(canDeleteRole);
                 SeedDB(context, uid);
             }
         }
 
-        private static async Task<string> CreateTestUser(IServiceProvider serviceProvider, string testUserPw)
+        private static string CreateTestUser(IServiceProvider serviceProvider, string testUserPw)
         {
             const string SeedUserName = "test@example.com";
 
             var userManager = serviceProvider.GetService<UserManager<ApplicationUser>>();
 
-            var user = await userManager.FindByNameAsync(SeedUserName);
+            var user =  userManager.FindByNameAsync(SeedUserName).Result;
             if (user == null)
             {
                 user = new ApplicationUser { UserName = SeedUserName };
-                await userManager.CreateAsync(user, testUserPw);
+                userManager.CreateAsync(user, testUserPw);
             }
 
             return user.Id;
-        }
+        }       
 
         private static async Task<IdentityResult> CreateCanDeleteRole(IServiceProvider serviceProvider, string uid, string canDeleteRole)
         {
@@ -61,14 +60,6 @@ namespace ContactManager.Data
             return ir;
         }
 
-        public static void AddRole(string canDeleteRole)
-        {
-            const string Issuer = "https://contoso.com";
-            var claims = new List<Claim>();
-            claims.Add(new Claim(ClaimTypes.Role, canDeleteRole, ClaimValueTypes.String, Issuer));
-            var userIdentity = new ClaimsIdentity("SuperSecureLogin");
-            userIdentity.AddClaims(claims);
-        }
 
         public static void SeedDB(ApplicationDbContext context, string uid)
         {
@@ -77,7 +68,8 @@ namespace ContactManager.Data
                 return;   // DB has been seeded
             }
 
-            context.Contact.AddRange(new Contact
+            context.Contact.AddRange(
+                new Contact
             {
                 Name = "Debra Garcia",
                 Address = "1234 Main St",
@@ -85,51 +77,50 @@ namespace ContactManager.Data
                 State = "WA",
                 Zip = "10999",
                 Email = "debra@example.com",
-                 OwnerID = uid
+                OwnerID = uid
             },
-    new Contact
-    {
-        Name = "Thorsten Weinrich",
-        Address = "5678 1st Ave W",
-        City = "Redmond",
-        State = "WA",
-        Zip = "10999",
-        Email = "thorsten@example.com",
-        OwnerID = uid
-    },
-    new Contact
-    {
-        Name = "Yuhong Li",
-        Address = "9012 State st",
-        City = "Redmond",
-        State = "WA",
-        Zip = "10999",
-        Email = "yuhong@example.com",
-        OwnerID = uid
-    },
-    new Contact
-    {
-        Name = "Jon Orton",
-        Address = "3456 Maple St",
-        City = "Redmond",
-        State = "WA",
-        Zip = "10999",
-        Email = "jon@example.com",
-        OwnerID = uid
-    },
-    new Contact
-    {
-        Name = "Diliana Alexieva-Bosseva",
-        Address = "7890 2nd Ave E",
-        City = "Redmond",
-        State = "WA",
-        Zip = "10999",
-        Email = "diliana@example.com",
-        OwnerID = uid
-    }
-    );
+             new Contact
+             {
+                 Name = "Thorsten Weinrich",
+                 Address = "5678 1st Ave W",
+                 City = "Redmond",
+                 State = "WA",
+                 Zip = "10999",
+                 Email = "thorsten@example.com",
+                 OwnerID = uid
+             },
+             new Contact
+             {
+                 Name = "Yuhong Li",
+                 Address = "9012 State st",
+                 City = "Redmond",
+                 State = "WA",
+                 Zip = "10999",
+                 Email = "yuhong@example.com",
+                 OwnerID = uid
+             },
+             new Contact
+             {
+                 Name = "Jon Orton",
+                 Address = "3456 Maple St",
+                 City = "Redmond",
+                 State = "WA",
+                 Zip = "10999",
+                 Email = "jon@example.com",
+                 OwnerID = uid
+             },
+             new Contact
+             {
+                 Name = "Diliana Alexieva-Bosseva",
+                 Address = "7890 2nd Ave E",
+                 City = "Redmond",
+                 State = "WA",
+                 Zip = "10999",
+                 Email = "diliana@example.com",
+                 OwnerID = uid
+             }
+             );
             context.SaveChanges();
-
         }
     }
 }
